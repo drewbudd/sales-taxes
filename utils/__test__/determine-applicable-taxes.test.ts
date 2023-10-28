a2a31ca8c8080ee85869e839d23f356118f72a81
Andrew Almaguer <andrew@almaguer.dev>
2023-10-28 21:23:21 +0200

    Implement determining if basic sales tax applies

--- utils/__test__/determine-applicable-taxes.test.ts
+++ utils/__test__/determine-applicable-taxes.test.ts
@@ -3,6 +3,17 @@ import determineApplicableTaxes from "../determineApplicableTaxes"
 describe('determineApplicableTaxes', () => {
   test.each([
     { input: 'book', expected: false },
+    { input: 'box of chocolates', expected: false },
+    { input: 'packet of headache pills', expected: false },
+    { input: 'imported bottles of perfume', expected: true },
+    { input: 'music CD', expected: true },
+    { input: 'imported', expected: true },
+  ])('determineApplicableTaxes given $input determines basicSalesTax=$expected', ({ input, expected }) => {
+    expect(determineApplicableTaxes(input).basicSalesTax).toEqual(expected)
+  })
+
+  test.each([
+    { input: 'book', expected: false },
     { input: 'imported bottles of perfume', expected: true },
     { input: 'import books', expected: false },
     { input: 'importedbooks', expected: true },
