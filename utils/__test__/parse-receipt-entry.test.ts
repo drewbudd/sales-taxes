a7de203f46cc715d202133ab3ed32dba0f0f84fe
Andrew Almaguer <andrew@almaguer.dev>
2023-10-28 18:28:27 +0200

    Implement parsing of receipt entry baseCost

--- utils/__test__/parse-receipt-entry.test.ts
+++ utils/__test__/parse-receipt-entry.test.ts
@@ -11,4 +11,14 @@ describe('parseReceiptEntry', () => {
   ])('parseReceiptEntry given $input parses quantity=$expectedQuantity', ({ input, expectedQuantity }) => {
     expect(parseReceiptEntry(input).quantity).toEqual(expectedQuantity)
   })
+
+  test.each([
+    { input: '1 book at 12.49', expectedBaseCost: 12.49 },
+    { input: '1 imported bottle of perfume at 27.99', expectedBaseCost: 27.99 },
+    { input: '1 imported bottle of perfume at 28', expectedBaseCost: 28 },
+    { input: '1 book at error', expectedBaseCost: NaN },
+    { input: '1 book at', expectedBaseCost: NaN },
+  ])('parseReceiptEntry given $input parses baseCost=$expectedBaseCost', ({ input, expectedBaseCost }) => {
+    expect(parseReceiptEntry(input).baseCost).toEqual(expectedBaseCost)
+  })
 })
