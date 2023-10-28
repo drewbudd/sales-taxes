07e3e9f0147aae6584b98465e33c8abb1f8d1158
Andrew Almaguer <andrew@almaguer.dev>
2023-10-28 21:23:21 +0200

    Implement determining if basic sales tax applies

--- utils/determineApplicableTaxes.ts
+++ utils/determineApplicableTaxes.ts
@@ -1,10 +1,11 @@
 import type { ApplicableTaxes } from "~/types/applicable-taxes";
+import BaseSalesTaxExemptions from "../data/base-sales-tax-exemptions";
 
 const IMPORT_IDENTIFIER = 'imported'
 
 export default function (description: string): ApplicableTaxes {
   return {
-    basicSalesTax: false,
+    basicSalesTax: !BaseSalesTaxExemptions.exceptionTermList().some(term => description.includes(term)),
     importSalesTax: description.includes(IMPORT_IDENTIFIER)
   }
 }
