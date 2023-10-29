40771a03702f0792de1647d890f05224e658b5bd
Andrew Almaguer <andrew@almaguer.dev>
2023-10-29 19:08:05 +0100

    Fix path alias resolving during testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vitest/config'
+import { resolve } from 'node:path'
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom'
+  },
+  resolve: {
+    alias: [
+      { find: "@", replacement: resolve(__dirname, ".") },
+      { find: "~", replacement: resolve(__dirname, ".") }
+    ]
   }
 })
