7eb1c593df88cfccb8a3189bfe60393867a4ca17
Andrew Almaguer <andrew@almaguer.dev>
2023-10-29 20:20:29 +0100

    Bugfix: Change calculateTax to round up to the nearest 0.05
    I incorrectly read "np/100 rounded up to the nearest 0.05" in the problem
    description. I was rounding to the nearest 0.05, not up to the nearest.

--- utils/calculateTax.ts
+++ utils/calculateTax.ts
@@ -1,10 +1,10 @@
 import type { ApplicableTaxes } from "~/types/applicable-taxes";
 
-const BASIC_SALES_TAX_RATE = 0.1
-const IMPORT_SALES_TAX_RATE = 0.05
+const BASIC_SALES_TAX_RATE = 10
+const IMPORT_SALES_TAX_RATE = 5
 
 export default function calculateTax(baseCost: number, applicableTaxes: ApplicableTaxes): number {
   const taxRate = (applicableTaxes.basicSalesTax ? BASIC_SALES_TAX_RATE : 0)
     + (applicableTaxes.importSalesTax ? IMPORT_SALES_TAX_RATE : 0)
-  return parseFloat((Math.round(((baseCost * taxRate) * 100) / 5) * 0.05).toFixed(2))
+  return parseFloat((Math.ceil((baseCost * taxRate) / 5) * 0.05).toFixed(2))
 }
